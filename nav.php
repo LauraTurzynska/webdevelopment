4086f51a8996e12c346fc937b59a5a011a5542f2
LauraTurzynska <laura2000110@gmail.com>
2022-10-27 09:45:37 +0200

    creating php

---
+++ nav.php
@@ -0,0 +1,11 @@
+<header>
+<a><img src="img/logo.jpeg" alt="logo"></a>
+<nav>
+	<ul>
+	<li><a href="#">Home</a></li>
+	<li><a href="#">About</a></li>
+	<li><a href="#">Products</a></li>
+	<li><a href="#">Contact</a></li>
+	</ul>
+	</nav>
+</header>
