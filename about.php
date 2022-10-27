3ee894cd118d5f33b845a47f5ea338191807ced1
LauraTurzynska <laura2000110@gmail.com>
2022-10-27 10:28:52 +0200

    adding about

--- about.php
+++ about.php
@@ -3,14 +3,14 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<title>Front page</title>
+<title>About</title>
 <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-	<?php
+		<?php
 	include "nav.php";
 	?>
-	<h1>Home</h1>
+	<h1>About</h1>
 </body>
 </html>
--- nav.php
+++ nav.php
@@ -2,9 +2,9 @@
 <a><img src="img/logo.jpeg" alt="logo"></a>
 <nav>
 	<ul>
-	<li><a href="#">Home</a></li>
-	<li><a href="#">About</a></li>
-	<li><a href="#">Products</a></li>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="about.php">About</a></li>
+	<li><a href="#">Prots</a></li>
 	<li><a href="#">Contact</a></li>
 	</ul>
 	</nav>
